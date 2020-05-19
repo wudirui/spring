21656ee4dd47b8d189dfdb0e0b12d34ea22e4a58
zhangrui <1570266831@qq.com>
2020-05-19 16:55:13 +0800

    调整工程目录

---
+++ src/main/java/com/zr/spring/injection/config/ConfigBean.java
@@ -0,0 +1,18 @@
+package com.zr.spring.injection.config;
+
+import com.zr.spring.injection.bean.UserBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ConfigBean {
+	@Bean(name = "userBean")
+	public UserBean userBean() {
+		UserBean userBean = new UserBean();
+		userBean.setName("无敌瑞");
+		userBean.setPwd("123456");
+		return userBean;
+	}
+}
